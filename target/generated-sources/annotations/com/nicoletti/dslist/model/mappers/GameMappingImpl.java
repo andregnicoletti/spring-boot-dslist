ff4b7d8216f653357dde8f1f9df659e3a5f44273
André Nicoletti <andregnicoletti@gmail.com>
2025-05-23 20:50:29 -0300

    fix profile variable default

--- target/generated-sources/annotations/com/nicoletti/dslist/model/mappers/GameMappingImpl.java
+++ target/generated-sources/annotations/com/nicoletti/dslist/model/mappers/GameMappingImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-05-23T20:04:07-0300",
+    date = "2025-05-23T20:49:58-0300",
     comments = "version: 1.6.3, compiler: javac, environment: Java 21.0.3 (Eclipse Adoptium)"
 )
 @Component
