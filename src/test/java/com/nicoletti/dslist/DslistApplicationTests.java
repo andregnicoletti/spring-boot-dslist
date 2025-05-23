3c17f81f6ce59a61ac4dfb48d88d8e998daa3b4f
André Nicoletti <andregnicoletti@gmail.com>
2025-05-23 09:02:05 -0300

    first commit

---
+++ src/test/java/com/nicoletti/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicoletti.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
